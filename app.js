3a631ab40be7fa1de260e42b2f9c3420c9a31bff
marcospg1026 <81495897+marcospg1026@users.noreply.github.com>
2021-04-11 21:02:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
-var main = function () { 
-   "use strict"; 
-   $(".comment-input button").on("click", function (event) { 
-      $(".comments").append("<p>this is a new comment</p>");  
-       }); 
-};
-$(document).ready(main);
-$(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>"); 
+var addCommentFromInputBox=function(){
+        $(".comment-input button").on("click",function(event){
+                var $new_comment = $("<p>");
+                var Input = $(".comment-input input").val();
+                if (Input== ""){
+                        window.alert("Comment section is empty!");
+                }
+                else{
+                        $new_comment.text(Input);
+                }
+                $new_comment.hide();
+                $(".comments").append($new_comment.fadeIn());
+                $(".comment-input input").val("");
+        });
 
-            $new_comment.text("this is another new comment");
-            var $new_comment = $("<p>").text("this is another new comment"); 
-            $(".comments").append($new_comment);
-            $(".comment-input input").val();
-});
+        $(".clear button").on("click", function (event){
+                $(".comment-input input").val("");
+        });
+
+        $(".remove button").on("click", function(event){
+                $("p").empty();
+        });
+
+
+        $(".comment-input input").on("clicks", function (event) {
+                var $new_comment=$("<p>");
+                var Input=$(".comment-input input").val();
+                if (Input== ""){
+                        window.alert("Comment section is empty!");
+                }
+                else{
+                        $new_comment.text(Input);
+                }
+                $new_comment.hide();
+                $(".comments").append($new_comment.fadeIn());
+                $(".comment-input input").val("");
+        });
+};
+var main = function () { 
+         "use strict";
+         addCommentFromInputBox();
+   };
+$(document).ready(main);
+
 
 
